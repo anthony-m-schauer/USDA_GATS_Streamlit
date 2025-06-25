67b620ecec194f93711271cd31d12a3d27006c95
anthony-m-schauer <anthony.m.schauer@gmail.com>
2025-06-25 16:09:20 -0500

    Update hhi_index.py

--- Working Streamlit For Cloud App/hhi_index.py
+++ Working Streamlit For Cloud App/hhi_index.py
@@ -13,18 +13,21 @@ warnings.filterwarnings('ignore', message='pandas only supports SQLAlchemy conne
 
 
 ##### Step 1: Get List of Trade Years
-def get_years_from_columns(cursor, table):
+def get_years_from_columns(cursor, table, schema="public"):
+    cursor.execute("""
+        SELECT column_name 
+        FROM information_schema.columns 
+        WHERE table_name = %s AND table_schema = %s
+    """, (table, schema))
     
-    conn = connect_to_sql()
-    cursor = conn.cursor()
-    cursor.execute(f"SHOW COLUMNS FROM {table}")
     columns = [col[0] for col in cursor.fetchall()]
     
     years = sorted([
         int(re.search(r'\d{4}', col).group())
         for col in columns
-        if col.startswith('value') and re.search(r'\d{4}', col)
+        if col and col.startswith('value') and re.search(r'\d{4}', col)
     ])
+    
     return years
 
 
@@ -33,7 +36,8 @@ def calculate_hhi(hs10_code, table):
     conn = connect_to_sql()
     cursor = conn.cursor()
     years = get_years_from_columns(cursor, table)
-    df = pd.read_sql(f"SELECT * FROM {table} WHERE hs10_code = %s", conn, params=(hs10_code,))
+    query = f"SELECT * FROM {table} WHERE hs10_code = '{hs10_code}' AND outlier IS NOT NULL"
+    df = pd.read_sql(query, conn)
 
     if df.empty:
         print(f"⚠️ No data found for HS10 code: {hs10_code}")
